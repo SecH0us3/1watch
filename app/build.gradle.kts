66c18c21bea211b7e7620ccb9c962e231d4cd414
SecH0us3 <github@ioc.slmail.me>
2026-08-22 11:12:27 +0300

    chore: bump version to 1.0.5 (code 5) for release bundle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.watch1.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
